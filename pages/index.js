3b3aea12f6a5e5ee8cd7899e2c3a471a0b0e015e
Rafael Capelo <rafaelcapelo22@gmail.com>
2022-01-17 17:36:18 -0300

    Início da construção da home top

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
 import Head from "next/head";
 import Image from "next/image";
-import Header from "../components/Header";
+import classes from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={classes.home}>
       <Head>
         <title>Pessoal Administradora</title>
       </Head>
-      <Header />
+      <div className={classes.home_top}>
+        <div className={classes.home_top_left}>
+          <div className={classes.home_top_left_logo}>
+            <h1>Logo</h1>
+          </div>
+          <div className={classes.home_top_left_access}>
+            <div className={classes.home_top_left_access_inputarea}>
+              <input
+                type="text"
+                className={classes.home_top_left_access_inputarea_input}
+                placeholder="Código do condomínio, ex. MDC"
+              />
+            </div>
+            <div className={classes.home_top_left_access_button}>Acessar</div>
+          </div>
+          <div className={classes.home_top_left_buttons}>
+            <div className={classes.home_top_left_buttons_button}>
+              <Image src={"/whatsapp.svg"} width={25} height={25} />
+            </div>
+            <div className={classes.home_top_left_buttons_button}>
+              <Image src={"/facebook.svg"} width={25} height={25} />
+            </div>
+            <div className={classes.home_top_left_buttons_button}>
+              <Image src={"/instagram.svg"} width={25} height={25} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.home_mid}></div>
+      <div className={classes.home_bottom}></div>
     </div>
   );
 }
