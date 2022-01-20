382233b1a3a8ef75a1b7afa0938c9b43e07333c4
Rycerax <rafaelcapelo22@gmail.com>
2022-01-20 00:41:40 -0300

    For building purpouse

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import classes from "../styles/Home.module.css";
 
 export default function Home() {
