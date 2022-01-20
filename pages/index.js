fef810adf2a4853729daa01a36376d3dbb11f581
Rafael Capelo <rafaelcapelo22@gmail.com>
2022-01-20 17:24:05 -0300

    Poucas mudanças

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                enable-background="new 0 0 24 24"
+                enableBackground="new 0 0 24 24"
                 width={27}
                 height={27}
                 viewBox="0 0 24 24"
