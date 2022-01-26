8b5cb9dfac5c8950ef34f1afaf998248e88b40b4
Rycerax <rafaelcapelo22@gmail.com>
2022-01-25 22:04:57 -0300

    Doing some tests

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
+  projectId: "pessoal-administradora-aa9a4",
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
