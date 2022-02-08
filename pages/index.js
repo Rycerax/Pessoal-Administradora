6d19444c6f3a9765165a53392fc9920e300b1bb1
Rafael Capelo <rafaelcapelo22@gmail.com>
2022-02-08 17:36:30 -0300

    fixing bugs

--- pages/index.js
+++ pages/index.js
@@ -245,7 +245,7 @@ export default function Home() {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  enable-background="new 0 0 24 24"
+                  enableBackground="new 0 0 24 24"
                   height={35}
                   viewBox="0 0 24 24"
                   width={35}
